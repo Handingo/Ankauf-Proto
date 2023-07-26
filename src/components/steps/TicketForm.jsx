74d37be04899d16b38bbd210c092ee9c9e72b137
Handingo <design@handingo.de>
2023-07-26 10:13:12 +0200

    Added temporary saving of ticket data

--- src/components/steps/TicketForm.jsx
+++ src/components/steps/TicketForm.jsx
@@ -4,25 +4,47 @@ import { connect } from "react-redux";
 
 
 class TicketForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    state = {
+        firstName: "",
+        lastName: "",
+        email: "",
+        demicile: "",
+        street: "",
+        houseNumber: ""
+    };
+
+    handleChange(e) {
+        e.preventDefault();
+
+        const {name, value} = e.currentTarget;
+        this.setState({ [name]: value });
+    }
+
     render() {
         return (
             <div id="ticket-form">
                 <label>Vorname</label>
-                <input placeholder="Vorname"/>
+                <input placeholder="Vorname" name="firstName" onChange={this.handleChange}/>
                 <label>Nachname</label>
-                <input placeholder="Nachname"/>
+                <input placeholder="Nachname" name="lastName" onChange={this.handleChange}/>
                 <label>E-Mail</label>
-                <input placeholder="E-Mail"/>
+                <input placeholder="E-Mail" name="email" onChange={this.handleChange}/>
                 <label>Wohnort</label>
-                <input placeholder="Wohnort"/>
+                <input placeholder="Wohnort" name="demicile" onChange={this.handleChange}/>
                 <label>Straße</label>
-                <input placeholder="Straße"/>
+                <input placeholder="Straße" name="street" onChange={this.handleChange}/>
                 <label>Hausnummer</label>
-                <input placeholder="Hausnummer"/>
+                <input placeholder="Hausnummer" name="houseNumber" onChange={this.handleChange}/>
                 <Button id="button-send-ticket">Senden</Button>
             </div>
         );
     }
 }
 
-export default connect()(TicketForm);
+export default connect(state => { return state; })(TicketForm);
