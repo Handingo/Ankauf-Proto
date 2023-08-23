815caf84e518cbdca4623ec3aa955cba8e5db058
Handingo <design@handingo.de>
2023-08-23 09:33:36 +0200

    Added testing app links to the ticket confirmation

--- src/components/steps/ticket/TicketConfirmation.tsx
+++ src/components/steps/ticket/TicketConfirmation.tsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import "./TicketConfirmation.css";
+import Link from "../../util/Link";
 
 class TicketConfirmation extends Component {
     render() {
@@ -10,7 +11,12 @@ class TicketConfirmation extends Component {
                     Du erhälst baldestmöglich eine Antwort per E-Mail.<br/>
                     Sofern die Antwort als Zusage eintrifft, schicken wir dir eine Sendungsmarke zu, mit deren Hilfe du uns dein Gerät schicken kannst.<br/>
                     <br/>
-                    <br/>
+                    Um dein Gerät noch einmal funktional zu testen, nutze bitte folgende App:
+                    <br/><br/>
+                    <Link text="NSYS Test für Android" href="https://play.google.com/store/apps/details?id=com.nsysgroup.nsystest"/>
+                    <br/><br/>
+                    <Link text="NSYS Test für Apple" href="https://apps.apple.com/de/app/nsys-test/id1448501054"/>
+                    <br/><br/>
                     - Dein Handingo-Team -<br/>
                     Am Schlangengraben 20<br/>
                     13597 Berlin<br/>
